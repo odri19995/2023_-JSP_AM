f81049e6d5f00ece15a1a7b5147e304842e45e4e
odri19995 <odri19995@gmail.com>
2023-04-05 15:01:18 +0900

    게시물 상세페이지 구현

--- src/main/java/com/koreaIT/java/am/ArticleDetailServlet.java
+++ src/main/java/com/koreaIT/java/am/ArticleDetailServlet.java
@@ -16,14 +16,12 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/article/list")
-public class ArticleListServlet extends HttpServlet {
+@WebServlet("/article/detail")
+public class ArticleDetailServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		response.setContentType("text/html; charset=UTF-8");
-		
+
 		Connection conn = null;
 
 		try {
@@ -31,18 +29,18 @@ public class ArticleListServlet extends HttpServlet {
 			String url = "jdbc:mysql://127.0.0.1:3306/JSPTest?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
 
 			conn = DriverManager.getConnection(url, "root", "");
-			
+
+			int id = Integer.parseInt(request.getParameter("id"));
+
 			SecSql sql = SecSql.from("SELECT * FROM article");
-			sql.append("ORDER BY id DESC");
-			
-			List<Map<String, Object>> articleListMap = DBUtil.selectRows(conn, sql);
-			
-//			response.getWriter().append(articleListMap.toString());
-			
-			request.setAttribute("articleListMap", articleListMap);
-			
-			request.getRequestDispatcher("/jsp/article/list.jsp").forward(request, response);
-			
+			sql.append("WHERE id = ?", id);
+
+			Map<String, Object> articleRow = DBUtil.selectRow(conn, sql);
+
+			request.setAttribute("articleRow", articleRow);
+
+			request.getRequestDispatcher("/jsp/article/detail.jsp").forward(request, response);
+
 		} catch (ClassNotFoundException e) {
 			System.out.println("드라이버 로딩 실패");
 		} catch (SQLException e) {
